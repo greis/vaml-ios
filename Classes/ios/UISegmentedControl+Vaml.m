bc9f9dfcf91d1c24c123a00612bcea4da11437e8
Gabriel Reis <gabriel.oreis+github@gmail.com>
2015-02-27 13:11:20 -0500

    Enable items for UISegmentedControl

--- Classes/ios/UISegmentedControl+Vaml.m
+++ Classes/ios/UISegmentedControl+Vaml.m
@@ -4,14 +4,19 @@
 @implementation UISegmentedControl (Vaml)
 
 -(id)initWithVamlData:(NSDictionary *)data context:(VamlContext *)context {
-  int i = 0;
-  NSString *item;
-  NSMutableArray *items = [NSMutableArray array];
-  while ((item = [data[@"attrs"] objectForKey:[NSString stringWithFormat:@"item%d", i]])) {
-    [items addObject:item];
-    i++;
+  NSDictionary *attrs = data[@"attrs"];
+  if (attrs[@"items"]) {
+    return [self initWithItems:[context.locals valueForKeyPath:attrs[@"items"]]];
+  } else {
+    int i = 0;
+    NSString *item;
+    NSMutableArray *items = [NSMutableArray array];
+    while ((item = [attrs objectForKey:[NSString stringWithFormat:@"item%d", i]])) {
+      [items addObject:item];
+      i++;
+    }
+    return [self initWithItems:items];
   }
-  return [self initWithItems:items];
 }
 
 @end
