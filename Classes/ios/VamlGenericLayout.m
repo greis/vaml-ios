7d998744c4770d8dd731ba170844030ae8dd9e93
Gabriel Reis <gabriel.oreis+github@gmail.com>
2015-02-27 13:09:17 -0500

    Only add constraints to VamlGenericLayout when subviews

--- Classes/ios/VamlGenericLayout.m
+++ Classes/ios/VamlGenericLayout.m
@@ -11,40 +11,42 @@
 -(void)updateConstraints {
   [self removeConstraints:self.constraints];
   
-  NSMutableDictionary *subviews = [NSMutableDictionary dictionary];
-  NSMutableArray *subviewsName = [NSMutableArray array];
-  
-  BOOL showHiddenChild = [@"true" isEqualToString:self.vamlAttrs[@"hiddenItems"]];
-  
-  [self setTranslatesAutoresizingMaskIntoConstraints:NO];
-  for (UIView *view in self.subviews) {
-    if (!showHiddenChild && view.hidden) { continue; }
-    [subviewsName addObject:view.vamlId];
-    subviews[view.vamlId] = view;
-    [view setTranslatesAutoresizingMaskIntoConstraints:NO];
-    [self addConstraint:[NSLayoutConstraint
-                         constraintWithItem:view
-                         attribute:self.alignmentAttribute
-                         relatedBy:NSLayoutRelationEqual
-                         toItem:self
-                         attribute:self.alignmentAttribute
-                         multiplier:1
-                         constant:self.alignmentPadding]];
-    [self addConstraint:[NSLayoutConstraint
-                         constraintWithItem:self
-                         attribute:self.dimensionAttribute
-                         relatedBy:NSLayoutRelationGreaterThanOrEqual
-                         toItem:view
-                         attribute:self.dimensionAttribute
-                         multiplier:1
-                         constant:2 * self.padding]];
+  if (self.subviews.count > 0) {
+    NSMutableDictionary *subviews = [NSMutableDictionary dictionary];
+    NSMutableArray *subviewsName = [NSMutableArray array];
+    
+    BOOL showHiddenChild = [@"true" isEqualToString:self.vamlAttrs[@"hiddenItems"]];
+    
+    [self setTranslatesAutoresizingMaskIntoConstraints:NO];
+    for (UIView *view in self.subviews) {
+      if (!showHiddenChild && view.hidden) { continue; }
+      [subviewsName addObject:view.vamlId];
+      subviews[view.vamlId] = view;
+      [view setTranslatesAutoresizingMaskIntoConstraints:NO];
+      [self addConstraint:[NSLayoutConstraint
+                           constraintWithItem:view
+                           attribute:self.alignmentAttribute
+                           relatedBy:NSLayoutRelationEqual
+                           toItem:self
+                           attribute:self.alignmentAttribute
+                           multiplier:1
+                           constant:self.alignmentPadding]];
+      [self addConstraint:[NSLayoutConstraint
+                           constraintWithItem:self
+                           attribute:self.dimensionAttribute
+                           relatedBy:NSLayoutRelationGreaterThanOrEqual
+                           toItem:view
+                           attribute:self.dimensionAttribute
+                           multiplier:1
+                           constant:2 * self.padding]];
+    }
+    
+    NSString *format = [NSString stringWithFormat:@"%@:|-padding-[%@]-padding-|",
+                        self.orientationForVisualFormat,
+                        [subviewsName componentsJoinedByString:@"]-spacing-["]];
+    id metrics = @{@"padding": @(self.padding), @"spacing": @(self.itemsSpacing)};
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:0 metrics:metrics views:subviews]];
   }
-  
-  NSString *format = [NSString stringWithFormat:@"%@:|-padding-[%@]-padding-|",
-                      self.orientationForVisualFormat,
-                      [subviewsName componentsJoinedByString:@"]-spacing-["]];
-  id metrics = @{@"padding": @(self.padding), @"spacing": @(self.itemsSpacing)};
-  [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:format options:0 metrics:metrics views:subviews]];
   
   [super updateConstraints];
 }
