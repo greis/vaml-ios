0f6f12de530a3b8e688cb0b87f08d4facd1e2b17
Gabriel Reis <gabriel.oreis@gmail.com>
2015-09-27 11:24:38 -0400

    Apply constraints to superview if parent is present on root

--- Classes/ios/VamlConstraintsHandler.m
+++ Classes/ios/VamlConstraintsHandler.m
@@ -6,15 +6,15 @@
 @property(nonatomic) UIView* rootView;
 @property(nonatomic) NSMutableArray *formats;
 @property(nonatomic) NSMutableDictionary *viewsDict;
+@property(nonatomic) BOOL useSuperviewAsRoot;
 @end
 
 
 @implementation VamlConstraintsHandler
 
-+(void)addConstraintsTo:(UIView *)rootView {
-  VamlConstraintsHandler *handler = [[self alloc] initWithRootView:rootView];
-  [handler populateFormatsForView:rootView];
-  [rootView addConstraintsWithFormats:handler.formats views:handler.viewsDict metrics:nil];
++(void)addConstraintsTo:(UIView *)view {
+  VamlConstraintsHandler *handler = [[self alloc] initWithRootView:view];
+  [handler apply];
 }
 
 #pragma mark - private
@@ -29,17 +29,28 @@
   return self;
 }
 
+-(void)apply {
+  [self populateFormatsForView:self.rootView];
+  UIView *targetView = self.useSuperviewAsRoot ? self.rootView.superview : self.rootView;
+  [targetView addConstraintsWithFormats:self.formats views:self.viewsDict metrics:nil];
+}
+
 -(void)populateFormatsForView:(UIView *)view {
   NSDictionary *attrs = view.vamlData.attrs;
   [attrs enumerateKeysAndObjectsUsingBlock:^(NSString *key, NSString *value, BOOL *stop) {
     if ([self isValidAttribute:key]) {
-      [self addView:view];
       if ([value rangeOfString:@"parent"].location != NSNotFound) {
+        
+        if (view == self.rootView) {
+          self.useSuperviewAsRoot = YES;
+        }
+        
         [self addView:view.superview];
         value = [value stringByReplacingOccurrencesOfString:@"parent" withString:[self viewId:view.superview]];
       }
+      [self addView:view];
       
-      value = [value stringByReplacingOccurrencesOfString:self.rootView.vamlId withString:@"superview"];
+      value = [value stringByReplacingOccurrencesOfString:self.rootView.vamlId withString:[self viewId:self.rootView]];
       
       NSString *constraint = [NSString stringWithFormat:@"%@.%@ == %@", [self viewId:view], key, value];
       [self.formats addObject:constraint];
@@ -74,13 +85,22 @@
 }
 
 -(void)addView:(UIView *)view {
-  if (view != self.rootView) {
+  if (view != self.rootView || (self.useSuperviewAsRoot && self.rootView.superview != nil)) {
     [self.viewsDict setObject:view forKey:view.vamlId];
   }
 }
 
 -(NSString *)viewId:(UIView *)view {
-  return view == self.rootView ? @"superview" : view.vamlId;
+  if (self.useSuperviewAsRoot) {
+    if (view == self.rootView.superview) {
+      return @"superview";
+    } else {
+      return view.vamlId;
+    }
+    
+  } else {
+   return view == self.rootView ? @"superview" : view.vamlId;
+  }
 }
 
 @end
