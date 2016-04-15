461c606512d33f85a7989cbd9717023ff83286a6
Gabriel Reis <gabriel.oreis@gmail.com>
2016-04-15 16:47:09 -0400

    Add alignment and keyboardType to UITextField

--- Classes/ios/UITextField+Vaml.m
+++ Classes/ios/UITextField+Vaml.m
@@ -8,6 +8,13 @@
   [self setPlaceholder:data[@"placeholder"]];
   [self setText:data[@"text"]];
   
+  [self setVamlAlignment:data];
+  [self setVamlKeyboardType:data];
+
+  return self;
+}
+
+-(void)setVamlAlignment:(VamlData *)data {
   NSString *alignment = data[@"alignment"];
   if ([@"left" isEqualToString:alignment]) {
     [self setTextAlignment:NSTextAlignmentLeft];
@@ -20,8 +27,14 @@
   } else if ([@"natural" isEqualToString:alignment]) {
     [self setTextAlignment:NSTextAlignmentNatural];
   }
+}
 
-  return self;
+-(void)setVamlKeyboardType:(VamlData *)data {
+  NSString *type = data[@"keyboardType"];
+  if ([@"number" isEqualToString:type]) {
+    [self setKeyboardType:UIKeyboardTypeNumberPad];
+  }
+  //TODO other types
 }
 
 @end
